03347f35eb3165d07c7b625a5710ad54ec2ca389
Abhi13394 <abhishek.koundal@belwo.com>
2026-06-26 16:41:32 +0000

    Create snapshot.bat

---
+++ scripts/snapshot.bat
@@ -0,0 +1,11 @@
+SET var=%cd%
+ECHO %var%
+
+set mydate=%date:~10,4%_%date:~4,2%_%date:~7,2%
+set mytime=%time:~0,2%_%time:~3,2%
+set filename=snapshot_before_import_%mydate%_%mytime%
+
+"C:\Program Files\Quadient\Inspire Designer 16.2.420 HF\InspireCLI.exe" -importpackage "C:\AutoDeploy\DeploymentAutomation\Scripts\SnapshotPackage\snapshot.pkg" -createrecoverysnapshot -recoverysnapshotlabel %filename%
+
+pause
+exit
