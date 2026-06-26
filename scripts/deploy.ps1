1ebf52d9910f92d42f6389f4e7815a8b9ff87471
Abhi13394 <abhishek.koundal@belwo.com>
2026-06-26 16:00:29 +0000

    Create deploy.ps1

---
+++ scripts/deploy.ps1
@@ -0,0 +1,53 @@
+param([string]$inputfile = "$PSScriptRoot\ChangeSets.csv", [int]$timeout = 15)
+
+Clear-Host
+$ErrorActionPreference = 'Stop'
+
+$baseDir = $PSScriptRoot
+$dateDir = "$baseDir\Export\$([datetime]::Now.ToString('yyyyMMdd'))"
+$csv = Get-Content -Path $inputfile
+$baseUrl = "http://localhost:30600/rest/api/submit-job/csexport?changeset=%changeset%&destination=%destination%"
+
+if (-not (Test-Path -Path $dateDir)) {
+    New-Item -ItemType Directory -Path $dateDir | Out-Null
+}
+
+Write-Host "Exporting $($csv.Length) changesets to $dateDir"
+
+$i = 0
+foreach($line in $csv) {
+    $i++
+    $changeset = $line.Split(',')[1]
+    $filename = $line.Split(',')[0]
+    if ([string]::IsNullOrWhiteSpace($filename)) {
+        $filename = "$changeset.chs"
+    }
+    $filepath = "$baseDir\$filename.chs"
+    $movepath = "$dateDir\$changeset.chs"
+    
+    if ([System.IO.File]::Exists($filepath)) { Remove-Item -Path $filepath }
+    if ([System.IO.File]::Exists($movepath)) { Remove-Item -Path $movepath }
+
+    try {
+        Write-Host "  [$($i.ToString().PadLeft(3, '0'))] Exporting changeset $changeset"
+        $url = $baseUrl.Replace("%changeset%", $changeset).Replace("%destination%", $baseDir)
+        $resp = Invoke-WebRequest -Uri $url
+
+        Start-Sleep -Seconds $timeout
+		Write-host "from $filepath to $movepath"
+        Move-Item -Path $filepath -Destination $movepath -Force
+        #Write-Host " * Exported changeset $changeset to $movepath"
+    }
+    catch {
+        Write-Warning $_
+        try {
+            Write-Warning "Error exporting $changeset to $filepath, message: $([System.Text.Encoding]::UTF8.GetString($resp.Content))"
+            Read-Host "Pausing, press any key..."
+        }
+        catch {
+            # meh
+        }
+    }
+}
+
+Read-Host "Finished, press any key..."
