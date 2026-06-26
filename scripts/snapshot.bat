ecd1768409171902ef9ba6ff73512210279efe41
Abhi13394 <abhishek.koundal@belwo.com>
2026-06-26 17:59:24 +0000

    Update snapshot.bat

--- scripts/snapshot.bat
+++ scripts/snapshot.bat
@@ -5,7 +5,7 @@ set mydate=%date:~10,4%_%date:~4,2%_%date:~7,2%
 set mytime=%time:~0,2%_%time:~3,2%
 set filename=snapshot_before_import_%mydate%_%mytime%
 
-"C:\Program Files\Quadient\Inspire Designer 17.3.437 HF\InspireCLI.exe" -importpackage "\\EC2AMAZ-EE21ATR\Storagefop\SnapshotPackage\snapshot.pkg" -createrecoverysnapshot -recoverysnapshotlabel %filename%
+"C:\Program Files\Quadient\Inspire Designer 17.3.437 HF\InspireCLI.exe" -importpackage "Z:\\SnapshotPackage\snapshot.pkg" -createrecoverysnapshot -recoverysnapshotlabel %filename%
 
 pause
 exit
