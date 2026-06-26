9bc0ea27a81582fd10c5fc167a99e7dd1904b42f
Abhi13394 <abhishek.koundal@belwo.com>
2026-06-26 16:50:31 +0000

    Create Import.ps1

---
+++ scripts/Import.ps1
@@ -0,0 +1,39 @@
+#param([string]$inputfile = "$PSScriptRoot\changeset\ChangeSets.csv", [int]$timeout = 5)
+param([string]$inputfile = "$PSScriptRoot\ChangeSets.csv", [int]$timeout = 5)
+
+#Clear-Host
+#$ErrorActionPreference = 'Stop'
+
+#$baseDir = $PSScriptRoot
+$baseDir = "C:\AutoDeploy\DeploymentAutomation\Scripts\"
+$dateDir = "$baseDir\Export\$([datetime]::Now.ToString('yyyyMMdd'))"
+#$dateDir = "$baseDir\Export\"
+$csv = Get-Content -Path $inputfile
+
+$i = 0
+foreach($line in $csv) {
+    $i++
+    $changeset = $line.Split(',')[0]
+    $filename = $line.Split(',')[1]
+
+    $exportpath = "$dateDir\$filename.chs"
+    $importpath = "$baseDir\Import\Staging\$filename.chs"
+    
+   try {
+        Copy-Item -Path $exportpath -Destination $importpath -Force
+        Write-Host " * Imported changeset $filename to $importpath"
+    }
+    catch {
+        Write-Warning $_
+        try {
+            Write-Warning "Error copying $filename.chs to $importpath, message: $([System.Text.Encoding]::UTF8.GetString($resp.Content))"
+            Read-Host "Pausing, press any key..."
+        }
+        catch {
+			# meh
+        }
+    }
+	Start-Sleep -Seconds 5
+}
+
+Read-Host "Finished, press any key..."
